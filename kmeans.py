0343a20d3417d132ddb8396137fe742b6af63fcc
Denis Caminha <dineresc@gmail.com>
2022-06-14 18:34:23 -0300

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -6,24 +6,31 @@ import matplotlib.pyplot as plt
 import seaborn
 
 from google.colab import files
-uploaded = files.upload()
-
 import io
+
+# Lendo o arquivo de entrada com as informacoes de (x,y)
+uploaded = files.upload()
 df = pandas.read_csv(io.BytesIO(uploaded['data.csv']))
 
 %matplotlib inline
 
-df.plot(kind='scatter',x='x',y='y',color="blue")
+# Plotando o gráfico inicial
+df.plot(kind='scatter',x='x',y='y',color="red")
 data_points = list(df.T.to_dict().values())
 
-def generate_clusters(centroide_1, centroide_2, centroide_3):
+def calcula_distancia_entre_pontos(p1,p2):
+  return math.sqrt(((p1['x'] - p2['x'])*(p1['x'] - p2['x'])) + ((p1['y'] - p2['y'])*(p1['y'] - p2['y'])))
+
+
+
+def gera_clusters(data_points, centroide_1, centroide_2, centroide_3):
 
   for idx,point in enumerate(data_points):
     
     # Calculando a distancia do ponto em questao para cada um dos centroides
-    d_1 = math.sqrt(((point['x'] - centroide_1['x'])*(point['x'] - centroide_1['x'])) + ((point['y'] - centroide_1['y'])*(point['y'] - centroide_1['y'])))
-    d_2 = math.sqrt(((point['x'] - centroide_2['x'])*(point['x'] - centroide_2['x'])) + ((point['y'] - centroide_2['y'])*(point['y'] - centroide_2['y'])))
-    d_3 = math.sqrt(((point['x'] - centroide_3['x'])*(point['x'] - centroide_3['x'])) + ((point['y'] - centroide_3['y'])*(point['y'] - centroide_3['y'])))
+    d_1 = calcula_distancia_entre_pontos(point, centroide_1)
+    d_2 = calcula_distancia_entre_pontos(point, centroide_2)
+    d_3 = calcula_distancia_entre_pontos(point, centroide_3)
     
     # Configura de qual cluster é aquele ponto, atraves da menor distancia entre
     # os tres possiveis
@@ -35,38 +42,25 @@ def generate_clusters(centroide_1, centroide_2, centroide_3):
       data_points[idx]['cluster'] = "3"
 
   return data_points
-# FIM metodo generate_clusters
+# FIM metodo gera_clusters
 
 
-init_centr_1 = randint(0,len(data_points))
-init_centr_2 = randint(0,len(data_points))
-init_centr_3 = randint(0,len(data_points))
 
-centroide_1 = data_points[init_centr_1]
-centroide_2 = data_points[init_centr_2]
-centroide_3 = data_points[init_centr_3]
-
-for i in range(50):
-
-  # Execucao da logica de setar o cluster de cada um dos pontos
-  data_points = generate_clusters(centroide_1, centroide_2, centroide_3)
-  
-  sum_x_cluster_1 = 0
-  sum_y_cluster_1 = 0
-  
-  sum_x_cluster_2 = 0
-  sum_y_cluster_2 = 0
-  
-  sum_x_cluster_3 = 0
-  sum_y_cluster_3 = 0
-  
-  qt_cluster_1 = 0
-  qt_cluster_2 = 0
-  qt_cluster_3 = 0
-
-
+def escolher_novos_centroides(data_point):
   # Somatorio dos valores de x,y para cada um dos pontos no data_points
   # para ser calculado o proximos candidatos a centroide
+  sum_x_cluster_1 = 0
+  sum_y_cluster_1 = 0
+  qt_cluster_1 = 0
+
+  sum_x_cluster_2 = 0
+  sum_y_cluster_2 = 0
+  qt_cluster_2 = 0
+
+  sum_x_cluster_3 = 0
+  sum_y_cluster_3 = 0  
+  qt_cluster_3 = 0
+
   for el in data_points:
     if el['cluster'] == "1":
       qt_cluster_1 += 1
@@ -80,23 +74,67 @@ for i in range(50):
       qt_cluster_3 += 1
       sum_x_cluster_3 += el['x']
       sum_y_cluster_3 += el['y']
-
-
   
+  print(qt_cluster_1, qt_cluster_3)
   # Identificacao dos novos centroides usando os somatorios do passo anterior
   new_centroide_1 = {'x': (sum_x_cluster_1 / qt_cluster_1), 'y': (sum_y_cluster_1/qt_cluster_1)}
   new_centroide_2 = {'x': (sum_x_cluster_2 / qt_cluster_2), 'y': (sum_y_cluster_2/qt_cluster_2)}
   new_centroide_3 = {'x': (sum_x_cluster_3 / qt_cluster_3), 'y': (sum_y_cluster_3/qt_cluster_3)}
 
-  # Verifica se os centroides estao se repedindo (se o novo centroide é igual ao antigo)
-  # Se for, termina o loop de procura de centroides
-  if new_centroide_1['x'] == centroide_1['x'] and new_centroide_1['y'] == centroide_1['y'] and new_centroide_2['x'] == centroide_2['x'] and new_centroide_2['y'] == centroide_2['y'] and new_centroide_3['x'] == centroide_3['x'] and new_centroide_3['y'] == centroide_3['y']:
-    break
-  else:
+  return new_centroide_1, new_centroide_2, new_centroide_3
+
+
+
+
+def possui_mudanca_de_centroides(nc1, c1, nc2, c2, nc3, c3):
+  if nc1['x'] == c1['x'] and nc1['y'] == c1['y'] and nc2['x'] == c2['x'] and nc2['y'] == c2['y'] and nc3['x'] == c3['x'] and nc3['y'] == c3['y']:
+    return False
+  return True
+
+
+
+#######################################
+#### INICIO DA LOGICA DO K-MEANS ######
+#######################################
+init_centr_1 = 0
+init_centr_2 = 0
+init_centr_3 = 0
+
+# Gerando centroides iniciais
+while init_centr_1 == init_centr_2 or init_centr_1 == init_centr_3 or init_centr_2 == init_centr_3:
+  init_centr_1 = randint(0,len(data_points)-1)
+  init_centr_2 = randint(0,len(data_points)-1)
+  init_centr_3 = randint(0,len(data_points)-1)
+
+centroide_1 = data_points[init_centr_1]
+centroide_2 = data_points[init_centr_2]
+centroide_3 = data_points[init_centr_3]
+
+
+# Loop de execucoes do k-means
+for i in range(50):
+
+  data_points = gera_clusters(data_points, centroide_1, centroide_2, centroide_3)
+  
+  new_centroide_1, new_centroide_2, new_centroide_3 = escolher_novos_centroides(data_points)  
+
+  if possui_mudanca_de_centroides(new_centroide_1, centroide_1, 
+                                  new_centroide_2, centroide_2, 
+                                  new_centroide_3, centroide_3):
     centroide_1 = new_centroide_1
     centroide_2 = new_centroide_2
     centroide_3 = new_centroide_3
+  else:
+    break
 
+
+
+
+# Configurando o dataframe para adicionar uma nova coluna referente ao clusters
+clusters = list(map(lambda x: x['cluster'], data_points))
+df['cluster'] = clusters
+
+seaborn.scatterplot(x='x', y='y', data=df, hue='cluster', ec=None)
 
 # Configurando o dataframe para adicionar uma nova coluna referente ao clusters
 clusters = list(map(lambda x: x['cluster'], data_points))
